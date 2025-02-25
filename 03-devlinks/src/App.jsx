545eca853fd128bcd422c7dcf0849d05e8a16b90
ProfCastello <gustavo.castello@sp.senai.br>
2025-02-25 16:49:45 -0300

    aula react vite criação de componentes

--- 03-devlinks/src/App.jsx
+++ 03-devlinks/src/App.jsx
@@ -1,14 +1,32 @@
+import "./App.css";
+
 import Perfil from "./components/perfil/perfil";
 import Switch from "./components/switch/Switch";
+import Links from "./components/links/Links";
+import SocialLinks from "./components/socialLinks/SocialLinks";
+import Rodape from "./components/rodape/Rodape";
 
 const App = () => {
   return (
-    <>
-      
-        <Perfil />
-        <Switch />
-  
-    </>
+    <div id="App" className="">
+      <Perfil />
+      <Switch />
+
+      <ul>
+        <Links />
+        <Links />
+        <Links />
+        <Links />
+      </ul>
+
+      <div id="socialLinks">
+        <SocialLinks />
+        <SocialLinks />
+        <SocialLinks />
+        <SocialLinks />
+      </div>
+      <Rodape />
+    </div>
   );
 };
 
