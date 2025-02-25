14bf277ca7fb80fb59ba3d148e72d7a04e4be123
ProfCastello <gustavo.castello@sp.senai.br>
2025-02-25 14:29:20 -0300

    I3DS-2025

---
+++ 03-devlinks/src/App.jsx
@@ -0,0 +1,15 @@
+import Perfil from "./components/perfil/perfil";
+import Switch from "./components/switch/Switch";
+
+const App = () => {
+  return (
+    <>
+      
+        <Perfil />
+        <Switch />
+  
+    </>
+  );
+};
+
+export default App;
