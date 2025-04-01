b20b2b3f9d15f7dc7928405f9402137de4e72334
ProfCastello <gustavo.castello@sp.senai.br>
2025-04-01 16:56:01 -0300

    Adicionado react-router com bootstrap e possibilidades de trabalho com copilot

---
+++ extra-devflix-bootstrap/src/App.jsx
@@ -0,0 +1,87 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import Footer from "./components/footer/Footer";
+import MovieCard from "./components/movieCard/MovieCard";
+import Logo from "./assets/devflix.png";
+import Lupa from "./assets/search.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap/scss/bootstrap.scss";
+import "./scss/styles.scss";
+
+const App = () => {
+  const colorScheme = () => {
+    const tema = window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
+
+    document.documentElement.setAttribute("data-bs-theme", tema);
+  };
+
+  colorScheme();
+
+  // Adiciona o evento de mudança de tema automaticamente
+  window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", colorScheme);
+
+  const [search, setSearch] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  //Utilizando chave de API do arquivo .env
+  // const apiKey = import.meta.env.VITE_OMDB_API_KEY;
+  const apiKey = "e4d577fa";
+  const apiUrl = `https://omdbapi.com/?apikey=${apiKey}`;
+
+  //Alimentando com dados para não ficar nulo com useEffect
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
+
+  //criando a conexão com a API e trazendo informações
+  const searchMovies = async (title) => {
+    const response = await fetch(`${apiUrl}&s=${title}`);
+    const data = await response.json();
+
+    //alimentando o movies
+    setMovies(data.Search);
+  };
+
+  //e = evento | ao clicar ou digitar acontece algo
+  const handleKeyPress = (e) => {
+    e.key === "Enter" && searchMovies(search);
+  };
+
+  return (
+    <div id="app">
+      <img className="logo" src={Logo} alt="" />
+
+      <div className="search">
+        <input
+          onKeyDown={handleKeyPress}
+          onChange={(e) => setSearch(e.target.value)}
+          type="text"
+          placeholder="Pesquise por filmes"
+        />
+        <img onClick={() => searchMovies(search)} src={Lupa} alt="" />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie, index) => (
+            <MovieCard key={index} apiUrl={apiUrl} {...movie} />
+          ))}
+        </div>
+      ) : (
+        <h2 className="empty">😢 Filme não encontrado 😢</h2>
+      )}
+
+      <Footer
+        devName={" ProfCastello"}
+        devLink={"https://github.com/ProfCastello"}
+      />
+    </div>
+  );
+};
+
+export default App;
