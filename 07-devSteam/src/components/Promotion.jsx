b55102dfbb52287840128e3451b289bbcaba21ae
ProfCastello <gustavo.castello@sp.senai.br>
2025-04-23 14:59:27 -0300

    carrinho com itens e tela promotion finalizada

--- 07-devSteam/src/components/Promotion.jsx
+++ 07-devSteam/src/components/Promotion.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PromoCard from "./PromoCard";
 
 const Promotion = (props) => {
+  const [aleatorio, setAleatorio] = useState([]);
+
   const games = [
     {
       id: 1,
@@ -69,31 +71,38 @@ const Promotion = (props) => {
     },
   ];
 
+  useEffect(() => {
+    const aleatorioJogos = games
+      .filter((jogo) => jogo.desconto > 0)
+      //.sort((a, b) => b.desconto - a.desconto) //ordenação por desconto decrescente
+      .sort(() => Math.random() - 0.5) //ordenação aleatória
+      .slice(0, 3);
+
+    setAleatorio(aleatorioJogos);
+  }, []);
+
   return (
     <div id="promotion" className="container w-75 my-4">
-      <h2 className="text-uppercase text-center text-md-start ms-md-5 ps-md-3 mb-4">Promoções</h2>
+      <h2 className="text-uppercase text-center text-md-start ms-md-5 ps-md-3 mb-4">
+        Promoções
+      </h2>
       <div
         id="itensPromo"
         className="d-flex flex-wrap gap-4 justify-content-around"
       >
         {/* mapeando um array com react */}
-        {games
-          .filter((jogo) => jogo.desconto > 0)
-          //.sort((a, b) => b.desconto - a.desconto) //ordenação por desconto decrescente
-          .sort(() => Math.random() - 0.5) //ordenação aleatória
-          .slice(0, 3)
-          .map((jogo) => (
-            <PromoCard
-              key={jogo.id}
-              titulo={jogo.titulo}
-              preco={jogo.preco.toFixed(2)}
-              desconto={jogo.desconto}
-              imagem={jogo.imagem}
-              //adicionando a opção de click com os itens do jogo no carrinho
-              onAddCarrinho={() => props.onAddCarrinho(jogo)}
-              //callback para adicionar somente um item e não todos os itens do array
-            />
-          ))}
+        {aleatorio.map((jogo) => (
+          <PromoCard
+            key={jogo.id}
+            titulo={jogo.titulo}
+            preco={jogo.preco.toFixed(2)}
+            desconto={jogo.desconto}
+            imagem={jogo.imagem}
+            //adicionando a opção de click com os itens do jogo no carrinho
+            onAddCarrinho={() => props.onAddCarrinho(jogo)}
+            //callback para adicionar somente um item e não todos os itens do array
+          />
+        ))}
       </div>
     </div>
   );
