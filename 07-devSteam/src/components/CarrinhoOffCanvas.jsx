6235638bdb8aeae4d22a5475d65ee0d63fef1372
ProfCastello <gustavo.castello@sp.senai.br>
2025-04-23 14:07:06 -0300

    ajustes botao finalizar compra

--- 07-devSteam/src/components/CarrinhoOffCanvas.jsx
+++ 07-devSteam/src/components/CarrinhoOffCanvas.jsx
@@ -10,7 +10,7 @@ const CarrinhoOffCanvas = (props) => {
   return (
     <div
       id="carrinhoOffCanvas"
-      className="offcanvas offcanvas-end show"
+      className="offcanvas offcanvas-end"
       style={{ background: "#364A63" }}
     >
       <div
@@ -93,7 +93,12 @@ const CarrinhoOffCanvas = (props) => {
               <strong>Total:</strong>
               <strong>R$ {total.toFixed(2)}</strong>
             </div>
-            <button className="btn btn-success w-100 mt-2">Finalizar Compra</button>
+            <button
+              id="addCarrinho"
+              className="btn btn-success desconto border-0 w-100 mt-2 fs-5"
+            >
+              Finalizar Compra
+            </button>
           </>
         )}
       </div>
